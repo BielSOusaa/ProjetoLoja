2bf68749be4322f2884b741b72278a2012c3410f
GABRIEL DE SOUSA CARVALHO FERREIRA <gabriel.scferreira@senacsp.edu.br>
2019-11-26 11:22:33 -0300

    Conluido os arquivos atualizar e apgar do contato

---
+++ data/contato/atualizar.php
@@ -0,0 +1,53 @@
+<?php
+
+    /*
+    Vamos construir os cabeçalhos para o trabalho com a api
+    */
+header("Access-Control-Allow-Origin:*");
+header("Content-Type: application/json; charset-utf-8");
+
+#Esse cabeçalho define o metodo de envio com PUT, ou sejá, como atualizar
+header("Access-Control-Allow-Methods:PUT");
+
+#Define o tempo de espera da api. Neste caso é 1 minuto.
+header("Access-Control-Max-Age:3600");
+
+include_once"../../config/database.php";
+
+include_once"../../domain/contato.php";
+
+$database = new Database();
+$db = $database->getConnection();
+
+$contato = new Contato($db);
+
+/*
+O cliente irá enviar os dados no formato json. Porém nós precisamos dos dados
+no formato php para cadastrar em banco de dados. Para realizar essa conversão
+iremos usar o banco json_decode. Assim o cliente enviar os dados. estes são
+lidos pela entrada php: e seu conteúdo é capturado e convertido para o formato
+php.
+*/
+$data = json_decode(file_get_contents("php://input"));
+
+#verificar se os campos estão com dados.
+if(!empty($data->telefone) && !empty($data->email) && !empty($data->id)){
+
+    $contato->telefone = $data->telefone;
+    $contato->email = $data->email;
+    $contato->id = $data->id;
+
+    if($contato->alterarContato()){
+        header("HTTP/1.0 201");
+        echo json_encode(array("mensagem"=>"Contato atualizado com sucesso!"));
+    }
+    else{
+        header("HTTP/1.0 400");
+        echo json_encode(array("mensagem"=>"Não foi possivel atualizar."));
+    }
+}
+else{
+    header("HTTP/1.0 400");
+    echo json_encode(array("mensagem"=>"Você precisa passar todos os dados para atualizar"));
+}
+?>
