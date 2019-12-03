9ded8a2db4429033da348aeac40e9bf131be142f
GABRIEL DE SOUSA CARVALHO FERREIRA <gabriel.scferreira@senacsp.edu.br>
2019-12-03 12:30:06 -0300

    Adição das imagens ao menu e inicio do css

--- Loja/src/app/app.component.ts
+++ Loja/src/app/app.component.ts
@@ -30,6 +30,11 @@ export class AppComponent {
       title: 'Sair',
       url: '/sair',
       icon: 'exit'
+    },
+    {
+      title: 'Politica',
+      url: '/politica',
+      icon: 'contact'
     }
   ];
 
