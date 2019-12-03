ed1a3a0cd5858bc322a46984b0650e774e6c46c2
GABRIEL DE SOUSA CARVALHO FERREIRA <gabriel.scferreira@senacsp.edu.br>
2019-12-03 09:51:51 -0300

    A pagina list foi removida

--- Loja/src/app/app-routing.module.ts
+++ Loja/src/app/app-routing.module.ts
@@ -12,10 +12,6 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
-    path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  },
-  {
     path: 'perfil',
     loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
   },
--- Loja/src/app/app.component.ts
+++ Loja/src/app/app.component.ts
@@ -17,11 +17,6 @@ export class AppComponent {
       icon: 'home'
     },
     {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
-    },
-    {
       title: 'Perfil',
       url: '/perfil',
       icon: 'person-add'
