289c5480e17f2dbfa5b0373c07d7840969eb38d5
GABRIEL DE SOUSA CARVALHO FERREIRA <gabriel.scferreira@senacsp.edu.br>
2019-12-11 10:24:35 -0300

    Alteração na pagina login. Preparando os campos e codigos para efetuar o login

--- Loja/src/app/login/login.page.ts
+++ Loja/src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +9,46 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router:Router) { }
+  private url:string = "http://localhost/dbloja/data/usuario/login.php";
+  public modelUs:Usuario;
+
+  constructor(private router:Router, private http:HttpClient) {
+    this.modelUs.usuario="";
+    this.modelUs.senha="";
+   }
 
   public cadastrar(){
     this.router.navigate(['/cadastrar']);
+  }
+
+  public efetuarlogin(){
+    var headers = new HttpHeaders();
+    headers.append("Accept","application/json");
+    headers.append("Content-Type","application/json");
+    headers.append("Access-Control-Allow-Origin","*");
+
+    var dados= {
+      "usuario":this.modelUs.usuario,
+      "senha":this.modelUs.senha
+    };
+
+    this.http.get(this.url,{headers:headers,params:dados}).subscribe(
+      data=>{
+        console.log(data);
+      },
+      error=>{
+        console.log("Erro ao tentar logar"+error);
+      }
+    );
+
   }
 
 
   ngOnInit() {
   }
 
+}
+export class Usuario{
+  usuario:string;
+  senha:string;
 }
